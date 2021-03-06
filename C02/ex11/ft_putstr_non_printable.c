c291c6ecc0eed4a183a840cc349bfbc9a11f5a42
Namwoo Kim <namwkim@c8r3s10.42seoul.kr>
2021-03-06 13:52:30 +0900

    123

---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: namwkim <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/23 11:09:49 by namwkim           #+#    #+#             */
+/*   Updated: 2021/02/25 21:26:03 by namwkim          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+char	g_arr[16];
+
+void	ft_hex(char input)
+{
+	write(1, "\\", 1);
+	write(1, &g_arr[input / 16], 1);
+	write(1, &g_arr[input % 16], 1);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int		index;
+	char	alpha;
+
+	index = -1;
+	alpha = '0';
+	while (++index < 10)
+		g_arr[index] = alpha++;
+	alpha = 'a';
+	while (index < 16)
+		g_arr[index++] = alpha++;
+	index = -1;
+	while (str[++index])
+	{
+		if (str[index] < ' ' || str[index] > '~')
+			ft_hex(str[index]);
+		else
+			write(1, &str[index], 1);
+	}
+}
