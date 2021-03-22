e705dd8faaef742110926cafe0a30ff879ff316a
tre2man <kimtree97@naver.com>
2021-03-22 23:58:47 +0900

    123

--- Rush00/src/rush01.c
+++ Rush00/src/rush01.c
@@ -11,9 +11,9 @@ void rush01(unsigned int x, unsigned int y)
         j = 1;
         while (j <= x)
         {
-            if (i == 1 && j == x || i == y && j == 1)
+            if ((i == 1 && j == x) || (i == y && j == 1))
                 ft_putchar('\\');
-            else if (i == 1 && j == 1 || i == y && j == x)
+            else if ((i == 1 && j == 1) || (i == y && j == x))
                 ft_putchar('/');
             else if (i == 1 || i == y || j == 1 || j == x)
                 ft_putchar('*');
--- Rush00/src/rush04.c
+++ Rush00/src/rush04.c
@@ -11,9 +11,9 @@ void rush04(unsigned int x, unsigned int y)
         j = 1;
         while (j <= x)
         {
-            if (i == 1 && j == 1 || i == y && j == x)
+            if ((i == 1 && j == 1) || (i == y && j == x))
                 ft_putchar('A');
-            else if (i == 1 && j == x || i == y && j == 1)
+            else if ((i == 1 && j == x) || (i == y && j == 1))
                 ft_putchar('C');
             else if (i == 1 || i == y || j == 1 || j == x)
                 ft_putchar('B');
